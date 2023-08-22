13352fba3d4551c909d8724a2c2fcedcfb668865
SURESHSPM <suresheeespm@gmail.com>
2023-08-22 13:19:05 +0530

    Doc updated

---
+++ ss.js
@@ -0,0 +1,15 @@
+// var countdown =0;
+// var i=10
+// while(i >= countdown)
+// {
+//     console.log(countdown)
+//     countdown++
+// }
+// var i = 5;
+// var countdown = 1;
+// do{
+//     console.log(countdown)
+//     countdown++
+// }
+// while(i >= countdown)
+console.log(Math.floor(Math.random()*800000));
